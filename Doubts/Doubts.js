ecd76107b8d2856efa37bfec5a7e1557e5c0e491
shubham2o <shubhamsharma.ha@gmail.com>
2022-07-12 23:01:33 +0530

    JavaScript Number toFixed() added

--- Doubts/Doubts.js
+++ Doubts/Doubts.js
@@ -22,3 +22,18 @@ const element = document.getElementById("myDIV");
 let text = "Height including padding and border: " + element.offsetHeight + "px<br>";
 text += "Width including padding and border: " + element.offsetWidth + "px";
 document.getElementById("demoOH").innerHTML = text;
+
+
+// JavaScript Number toFixed()
+// EXAMPLE 1
+let num = 5.56789;
+let n = num.toFixed();
+
+document.getElementById("demoTF").innerHTML = n;
+
+
+// EXAMPLE 2
+let num2 = 5.56789;
+let n2 = num.toFixed(2);
+
+document.getElementById("demoTF1").innerHTML = n2;
