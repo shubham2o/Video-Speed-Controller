9bc789bf53076c288e7bd6d609a5b82cff2d97ee
shubham2o <shubhamsharma.ha@gmail.com>
2022-07-12 22:52:20 +0530

    HTML DOM Element offsetHeight added

--- Doubts/Doubts.js
+++ Doubts/Doubts.js
@@ -14,3 +14,11 @@ function offsetTop1() {
     var testDiv = document.getElementById("test");
     document.getElementById("demoOT").innerHTML = testDiv.offsetTop;
 }
+
+
+// HTML DOM Element offsetHeight
+// EXAMPLE 1
+const element = document.getElementById("myDIV");
+let text = "Height including padding and border: " + element.offsetHeight + "px<br>";
+text += "Width including padding and border: " + element.offsetWidth + "px";
+document.getElementById("demoOH").innerHTML = text;
